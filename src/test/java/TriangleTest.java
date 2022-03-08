4624b0fe6490a4662e53f2bfb61e0033188f87c2
adriguerre <adrianguerreroalvarez@gmail.com>
2022-03-08 11:34:45 +0100

    Added Test

---
+++ src/test/java/TriangleTest.java
@@ -0,0 +1,83 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TriangleTest {
+
+        private Triangle triangulo;
+        @BeforeEach
+        public void setup(){
+            triangulo = new Triangle();
+        }
+
+        @AfterEach
+        public void finish(){
+            triangulo = null;
+        }
+
+        @Test
+        public void testComputeReturnExceptionWhenOneSideIsNegative(){
+            assertThrows(RuntimeException.class, () -> triangulo.getType(-1, 2, 3));
+        }
+
+        @Test
+        public void testComputeReturnExceptionWhenOneSideIsZero(){
+            assertThrows(RuntimeException.class, () -> triangulo.getType(0, 2, 3));
+        }
+
+        @Test
+        public void testComputeReturnExceptionWhenOneSideIsZero2() {
+            assertThrows(RuntimeException.class, () -> triangulo.getType(2, 0, 3));
+        }
+
+        @Test
+        public void testComputeReturnExceptionWhenOneSideIsZero3() {
+            assertThrows(RuntimeException.class, () -> triangulo.getType(2, 3, 0));
+        }
+
+        @Test
+        public void testComputeReturnExceptionWhenAllSidesAreZero() {
+            assertThrows(RuntimeException.class, () -> triangulo.getType(0, 0, 0));
+        }
+
+        @Test
+        public void testComputeReturnExceptionWhenTriangleIsNotValid(){
+            assertThrows(RuntimeException.class, () -> triangulo.getType(2, 3, 5));
+        }
+
+        @Test
+        public void testComputeReturnEquilateralWhenTriangleHasAllSidesEqual(){
+            assertEquals(Triangle.TriangleType.EQUILATERAL, triangulo.getType(2, 2, 2));
+        }
+
+        @Test
+        public void testComputeReturnIsoscelesWhenTriangleHasTwoSidesEquals(){
+            assertEquals(Triangle.TriangleType.ISOSCELES, triangulo.getType(2, 2, 1));
+        }
+
+        @Test
+        public void testComputeReturnIsoscelesWhenTriangleHasTwoSidesEqualsNumbersPermutation(){
+            assertEquals(Triangle.TriangleType.ISOSCELES, triangulo.getType(1, 2, 2));
+        }
+
+        @Test
+        public void testComputeReturnIsoscelesWhenTriangleHasTwoSidesEqualsNumbersPermutation2(){
+            assertEquals(Triangle.TriangleType.ISOSCELES, triangulo.getType(2, 1, 2));
+        }
+        @Test
+        public void testComputeReturnScaleneWhenTriangleHasAllSidesDifferent(){
+            assertEquals(Triangle.TriangleType.SCALENE, triangulo.getType(4, 2, 3));
+        }
+
+        @Test
+        public void testComputeReturnNotEquilateralWhenTriangleIsNotEquilateral(){
+            assertNotEquals(Triangle.TriangleType.EQUILATERAL, triangulo.getType(2, 3, 4));
+        }
+
+
+
+
+
+}
